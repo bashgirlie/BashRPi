4fd3306bc9fa9de7b580e736a6a02fc27f4116d3
bashgirlie <94557891+bashgirlie@users.noreply.github.com>
2026-07-07 11:17:46 -0500

    Update nmapper.py
    starting with basics from BH python

--- recon/nmapper.py
+++ recon/nmapper.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python
 
+import socket
+import os
 
+# host to listen on
+HOST = '0.0.0.0'
+
+def main():
+    # create raw socket, bin to public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+    #include IP header in capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # read a packet
+    print(sniffer.recvfrom(65565))
+
+    #if on windows turn off promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+if __name__ == '__main__':
+    main()
